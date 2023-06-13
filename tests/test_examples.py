3d96ff123868130d0591362b717d9ab5bc81f473
Tetsuo Koyama <tkoyama010@gmail.com>
2023-06-14 08:27:47 +0900

    Fix error

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -4,7 +4,6 @@ import pytest
 
 import pyvista as pv
 from pyvista import examples
-from pyvista.core.errors import PyVistaDeprecationWarning
 
 
 def test_sphere_with_texture_map():
@@ -18,5 +17,4 @@ def test_load_earth():
     mesh = pv.examples.planets.load_earth()
     assert isinstance(mesh, pv.PolyData)
     assert mesh.n_cells
-    with pytest.warns(PyVistaDeprecationWarning):
-        assert mesh.textures["surface"]
+    assert mesh.textures["surface"]
