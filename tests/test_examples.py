2daa4e47dc1eaef0328cc3b2c81813c65b118d22
Tetsuo Koyama <tkoyama010@gmail.com>
2023-06-14 08:39:31 +0900

    :white_check_mark: Add tests that move from pyvista to pyvista-planets (#1)
    * Copy from pyvista tests
    
    * Remove unused tests
    
    * Add test.yml
    
    * Add requirements.txt
    
    * Update requirements.txt
    
    * Fix error
    
    * Add test_download_files.py

---
+++ tests/test_examples.py
@@ -0,0 +1,20 @@
+"""Test examples that do not require downloading."""
+import numpy as np
+import pytest
+
+import pyvista as pv
+from pyvista import examples
+
+
+def test_sphere_with_texture_map():
+    sphere = pv.examples.planets._sphere_with_texture_map()
+    assert isinstance(sphere, pv.PolyData)
+    assert 'Texture Coordinates' in sphere.point_data
+    assert sphere['Texture Coordinates'].shape == (sphere.n_points, 2)
+
+
+def test_load_earth():
+    mesh = pv.examples.planets.load_earth()
+    assert isinstance(mesh, pv.PolyData)
+    assert mesh.n_cells
+    assert mesh.textures["surface"]
