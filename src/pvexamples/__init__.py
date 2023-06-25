1f350ac80069aa43c92758e2000bf73a1cd2d314
Tetsuo Koyama <tkoyama010@gmail.com>
2023-06-25 12:02:19 +0900

    Copy from pyvista/examples

--- src/pvexamples/__init__.py
+++ src/pvexamples/__init__.py
@@ -1,4 +1,7 @@
 """Examples module."""
-# flake8: noqa: F401
-
+from pyvista.examples.downloads import *
+from pyvista.examples.examples import *
 from . import planets
+from . import gltf
+from . import vrml
+from .cells import plot_cell
