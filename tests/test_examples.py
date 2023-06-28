3c396cd9ee9a4a39a68e687e055be27b6467a761
Tetsuo Koyama <tkoyama010@gmail.com>
2023-06-28 12:04:55 +0900

    :truck: Move `examples` from `pyvista.examples` (#13)
    * Add tests
    
    * Update test_examples.py
    
    * Add examples.py
    
    * Create requirements_test.txt
    
    * Update requirements.txt
    
    * Update requirements_test.txt
    
    * Update test.yml
    
    * Add example files

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,17 +1,81 @@
 """Test examples that do not require downloading."""
+import numpy as np
+import pytest
+
 import pyvista as pv
-from pvexamples import planets
+import pvexamples as examples
+
+
+def test_load_nut():
+    mesh = examples.load_nut()
+    assert mesh.n_points
+
+
+def test_load_ant():
+    """Load ply ant mesh"""
+    mesh = examples.load_ant()
+    assert mesh.n_points
+
+
+def test_load_airplane():
+    """Load ply airplane mesh"""
+    mesh = examples.load_airplane()
+    assert mesh.n_points
+
+
+def test_load_sphere():
+    """Loads sphere ply mesh"""
+    mesh = examples.load_sphere()
+    assert mesh.n_points
+
+
+def test_load_channels():
+    """Loads geostat training image"""
+    mesh = examples.load_channels()
+    assert mesh.n_points
+
+
+def test_load_spline():
+    mesh = examples.load_spline()
+    assert mesh.n_points
+
+
+def test_load_random_hills():
+    mesh = examples.load_random_hills()
+    assert mesh.n_cells
+
+
+def test_load_tetbeam():
+    mesh = examples.load_tetbeam()
+    assert mesh.n_cells
+    assert (mesh.celltypes == 10).all()
 
 
 def test_sphere_with_texture_map():
-    sphere = planets._sphere_with_texture_map()
+    sphere = examples.planets._sphere_with_texture_map()
     assert isinstance(sphere, pv.PolyData)
     assert 'Texture Coordinates' in sphere.point_data
     assert sphere['Texture Coordinates'].shape == (sphere.n_points, 2)
 
 
 def test_load_earth():
-    mesh = planets.load_earth()
+    mesh = examples.planets.load_earth()
     assert isinstance(mesh, pv.PolyData)
     assert mesh.n_cells
     assert mesh.textures["surface"]
+
+
+def test_load_hydrogen_orbital():
+    with pytest.raises(ValueError, match='`n` must be'):
+        examples.load_hydrogen_orbital(-1, 1, 0)
+    with pytest.raises(ValueError, match='`l` must be'):
+        examples.load_hydrogen_orbital(1, 1, 0)
+    with pytest.raises(ValueError, match='`m` must be'):
+        examples.load_hydrogen_orbital(1, 0, 1)
+
+    orbital = examples.load_hydrogen_orbital(3, 2, 1)
+    assert isinstance(orbital, pv.UniformGrid)
+    assert 'wf' in orbital.point_data
+    assert orbital.point_data['wf'].dtype == np.complex128
+    assert 'real_wf' in orbital.point_data
+    assert orbital.point_data['real_wf'].dtype == np.float64
