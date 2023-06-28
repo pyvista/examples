0b38c092551dfb92fd4006b2d6110a02336c5585
Tetsuo Koyama <tkoyama010@gmail.com>
2023-06-28 13:11:40 +0900

    Update __init__.py

--- src/pvexamples/__init__.py
+++ src/pvexamples/__init__.py
@@ -1,12 +1,8 @@
 """Examples module."""
-<<<<<<< HEAD
-from pyvista.examples.downloads import *
-from pyvista.examples.examples import *
-=======
 # flake8: noqa: F401
 
-from pvexamples.examples import *
->>>>>>> 3c396cd9ee9a4a39a68e687e055be27b6467a761
+from pyvista.examples.downloads import *
+from pyvista.examples.examples import *
 from . import planets
 from . import gltf
 from . import vrml
