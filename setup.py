6f7a8171cbdf8e65143be8345ca4337affe44e36
Tetsuo Koyama <tkoyama010@gmail.com>
2023-07-03 16:15:07 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='an_example_package',
+    name='pyvista-examples',
     # other arguments omitted
     long_description=long_description,
     long_description_content_type='text/markdown'
