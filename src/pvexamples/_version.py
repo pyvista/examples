7d93056c446e00e7c78064dd10785b56c474322a
Tetsuo Koyama <tkoyama010@gmail.com>
2023-07-04 11:28:03 +0900

    :bookmark: Add package version number (#22)
    * :bookmark: Add package version number
    
    * Update __init__.py
    
    * Update pyproject.toml
    
    * Update _version.py
    
    * Update _version.py

---
+++ src/pvexamples/_version.py
@@ -0,0 +1,23 @@
+"""Version info for pyvista-examples.
+
+On the ``main`` branch, use 'dev' to denote a development version.
+For example:
+
+version_info = 0, 27, 0, now.year, now.month, now.day, now.hour, now.minute, now.second
+
+When generating pre-release wheels, use '0rcN', for example:
+
+version_info = 0, 28, '0rc1'
+
+Denotes the first release candidate.
+
+"""
+import datetime
+
+now = datetime.datetime.now()
+
+# major, minor, patch
+version_info = 0, 0, 0, now.year, now.month, now.day, now.hour, now.minute, now.second
+
+# Nice string for the version
+__version__ = '.'.join(map(str, version_info))
