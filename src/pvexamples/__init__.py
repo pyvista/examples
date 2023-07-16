d7ab02b9eff2451a2add06513b5e6184166e48fe
Tetsuo Koyama <tkoyama010@gmail.com>
2023-07-16 15:53:32 -0500

    Update __init__.py

--- src/pvexamples/__init__.py
+++ src/pvexamples/__init__.py
@@ -1,7 +1,10 @@
 """Examples module."""
 # flake8: noqa: F401
 
-from pvexamples import gltf, planets, vrml
-from pvexamples.cells import plot_cell
 from pvexamples.downloads import *
 from pvexamples.examples import *
+from . import planets
+from . import gltf
+from . import vrml
+from .cells import plot_cell
+from pvexamples._version import __version__
